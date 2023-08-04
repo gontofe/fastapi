1224a31dff4747254603fb6c3d217e52116b0cd5
Mike Hall <michaelrichardhall@gmail.com>
2023-08-04 17:03:07 +0100

    add models endpoint

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
 
 # needs to be first, so 'me' doesn't get interpreted as a user ID
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
+
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
 
 @app.get("/users")
 async def read_users():
     return ["Rick", "Morty"]
 
+
 # won't actually get hit, because the function above will override
 @app.get("/users")
 async def read_users():
     return ["Bean", "Elfo"]
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
