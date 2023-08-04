4b6ce6e07d5b13544fb150e11f065dad2d3cbe64
Mike Hall <michaelrichardhall@gmail.com>
2023-08-04 17:01:36 +0100

    add users endpoints

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+# needs to be first, so 'me' doesn't get interpreted as a user ID
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+# won't actually get hit, because the function above will override
+@app.get("/users")
+async def read_users():
+    return ["Bean", "Elfo"]
