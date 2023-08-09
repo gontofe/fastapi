1bd197afd83833a98a404eda61322be28f1f7452
Mike Hall <michaelrichardhall@gmail.com>
2023-08-09 15:51:19 +0100

    add mixture of required and optional params

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ async def read_item(user_id: int, item_id: int, q: str | None = None, short: boo
 
 
 @app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, needy: needy}
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
 
 
