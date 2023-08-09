e1f2ed82d5d21c11064ce5804c9ee8fdbdef2fe7
Mike Hall <michaelrichardhall@gmail.com>
2023-08-09 16:16:44 +0100

    use path param and body at the same time

--- main.py
+++ main.py
@@ -94,3 +94,7 @@ async def create_item(item: Item):
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
